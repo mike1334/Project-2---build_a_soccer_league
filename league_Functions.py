c66163fa65382397fe78ed55ac1ffa7e1dcd17dc
Bone Snapper <bonesnapper@Bones-MacBook-Pro.local>
2019-03-20 15:12:06 -0400

    Added comments, Fixed txt output

--- league_Functions.py
+++ league_Functions.py
@@ -1,12 +1,18 @@
 import csv
+import copy
 
 teams = {
-				'Sharks':[],
-				'Dragons':[],
-				'Raptors':[]
-				}
+		'Sharks':[],
+		'Dragons':[],
+		'Raptors':[]
+		}
 
 def divide_teams_experience():
+
+# This function loops through the provided .csv file. First it finds and splits evenly the experinenced 
+# players between the three teams. Next it accomplishes the same task with the inexperienced players.
+# the results are stored in the teams{} for future reference and use.
+
 	with open("soccer_players.csv", 'r') as player_csv:
 		fieldnames = ['Name','Height (inches)','Soccer Experience','Guardian Name(s)']
 		reader = csv.DictReader(player_csv)
@@ -39,34 +45,39 @@ def divide_teams_experience():
 
 
 def build_teams():
-	with open("roster_master.txt",'w') as roster:
+
+# This function firstly creates the teams.txt output file. The first loop goes through a copy of the teams{} dictionary 
+# and .pop()'s the unnecessary key fields from each teams list. The copy is used as to not modify the original global teams{}
+# for future use. In loops the team name is printed first before its players are written, once the end of team list is reached
+# the next loop starts for the following team.
+
+	with open("teams.txt",'w') as roster:
 		fieldnames = ['Name','Soccer Experience','Guardian Name(s)']
-
 		roster_w = csv.DictWriter(roster, fieldnames = fieldnames)
-		
-		
-		
-		for l in range(len(teams['Sharks'])):
 
-			teams['Sharks'][l].pop('Height (inches)')
-			teams['Dragons'][l].pop('Height (inches)')
-			teams['Raptors'][l].pop('Height (inches)')
+		# .deepcopy(x,[ memo]) used here to make a copy of the dict() and all of the elements within it, 		
+		teams_copy = copy.deepcopy(teams) 
+		
+		
+		for l in range(len(teams_copy['Sharks'])):
+
+			teams_copy['Sharks'][l].pop('Height (inches)')
+			teams_copy['Dragons'][l].pop('Height (inches)')
+			teams_copy['Raptors'][l].pop('Height (inches)')
 			
-
-
 		roster.write('\nSharks\n')
-		for l in teams['Sharks']:
+		for l in teams_copy['Sharks']:
 			roster_w.writerow(l)
 
 		roster.write('\nDragons\n')
-		for l in teams['Dragons']:
+		for l in teams_copy['Dragons']:
 			roster_w.writerow(l)
 
 		roster.write('\nRaptors\n')
-		for l in teams['Raptors']:
+		for l in teams_copy['Raptors']:
 			roster_w.writerow(l)
 
-
+		teams_copy.clear()
 
 
 
--- league_builder.py
+++ league_builder.py
@@ -7,8 +7,8 @@ import league_Functions
 
 if __name__ == "__main__":
 
-	league_Functions.divide_teams_experience()
-	league_Functions.build_teams()
+	league_Functions.divide_teams_experience() # from league_functions split up the teams evenly by experience.
+	league_Functions.build_teams() # from league_functions build teams.txt and seperate players by assigned team.
 
 		
 
